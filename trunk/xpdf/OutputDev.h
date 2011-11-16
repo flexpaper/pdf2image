cd5d216cc0ca40d18e2a5fbf79eb23795b7b4218
pdf2image@devaldi.com <pdf2image@devaldi.com@d0a1146c-0457-f6f2-2b18-5d3df21b819d>
2011-11-16 00:56:32 +0000

    0.50

--- trunk/xpdf/OutputDev.h
+++ trunk/xpdf/OutputDev.h
@@ -94,7 +94,7 @@ public:
     { return gTrue; }
 
   // Start a page.
-  virtual void startPage(int pageNum, GfxState *state) {}
+  virtual void startPage(int pageNum, GfxState *state, double x1,double y1,double x2,double y2) {}
 
   // End a page.
   virtual void endPage() {}
